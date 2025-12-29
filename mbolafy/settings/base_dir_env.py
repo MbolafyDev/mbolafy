011080aea454d7af8dfde68b1b8bfeba2c018418
MbolafyDev <mbolafy.dev@gmail.com>
2025-12-29 14:00:37 +0300

    changement du base_env_dir.py

--- mbolafy/settings/base_dir_env.py
+++ mbolafy/settings/base_dir_env.py
@@ -1,17 +1,20 @@
-# mbolafy/mbolafy/settings/base_dir_env.py
-
 from pathlib import Path
 import os
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# Exemple:
-# mbolafy.settings.dev  -> .env.dev
-# mbolafy.settings.prod -> .env.prod
-settings_module = os.getenv('DJANGO_SETTINGS_MODULE', 'mbolafy.settings.dev')
+# Forcer DJANGO_SETTINGS_MODULE par défaut
+settings_module = os.getenv(
+    'DJANGO_SETTINGS_MODULE',
+    'mbolafy.settings.prod'
+)
+
 env_suffix = settings_module.split('.')[-1]
 
 ENV_FILE = BASE_DIR / f'.env.{env_suffix}'
 
-load_dotenv(ENV_FILE)
+if ENV_FILE.exists():
+    load_dotenv(ENV_FILE)
+else:
+    raise FileNotFoundError(f"❌ Fichier {ENV_FILE} introuvable")
